c9cccdd41e717ca99ae46a4d84fc0cd47ccdad66
Maciej Olko <maciej.olko@yougov.com>
2020-09-01 20:23:05 +0200

    Add fallback test
    Closes #3

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from unittest.mock import Mock
 
 from translations import AttributiveTranslations
+from translations import NoContextFallbackTranslations
 
 empty_mo_file = BytesIO(b'\x95\x04\x12\xde'  # magic number, same as gettext.GNUTranslations.LE_MAGIC.to_bytes(4, 'big')
                         b'\x00\x00\x00\x01'  # version
@@ -26,3 +27,12 @@ class AttributiveTranslationsTest(TestCase):
         foo.attribute
 
         t.pgettext.assert_called_once_with('attribute', 'foo')
+
+    def test_fallbacks(self):
+        t = NoContextFallbackTranslations()
+        t.gettext = Mock()
+        t._catalog = {}
+
+        t.pgettext('samplecontext', 'samplemessage')
+
+        t.gettext.assert_called_once_with('samplemessage')
